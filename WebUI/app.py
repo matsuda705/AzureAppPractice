d4648665ad5b7a637fd39dcd95bc02133fb85bc9
matsuda705 <koji.matsuda@jbs.com>
2024-07-09 01:20:34 +0000

    テスト用api追加、自動更新機能追加

--- WebUI/app.py
+++ WebUI/app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+import json
+import os
+
+from flask import Flask, render_template, jsonify
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+DUMMY_DATA_PATH = os.path.join(current_dir, "dummy/dummy_data.json")
 
 app = Flask(__name__, static_folder="./statics")
 
@@ -6,6 +12,25 @@ app = Flask(__name__, static_folder="./statics")
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/get_dummy')
+def get_dummy():
+    data = _get_dummy_data()
+    return jsonify(data)
+
+
+def _get_dummy_data():
+    """
+    テスト用 ダミーのjsonデータを取得して返す
+    """
+    try:
+        with open(DUMMY_DATA_PATH, 'r') as file:
+            data = json.load(file)
+        return data
+    except Exception() as e:
+        print(e)
+        return []
 
 
 if __name__ == '__main__':
---
+++ WebUI/statics/js/index.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+
+    function updateAirconCard($card_unit, unit_data) {
+        // エアコンカード部分の更新
+        $status_val = $card_unit.find(".status-val");
+        $card_unit.find(".set-temp").text(unit_data["temp"] + " ℃");
+
+        $status_val.removeClass('heater');
+        $status_val.removeClass('cooler');
+        if (unit_data["mode"] === "heat") {
+            $status_val.text("暖房");
+            $status_val.addClass('heater');
+        } else {
+            $status_val.text("冷房");
+            $status_val.addClass('cooler');
+        }
+    }
+
+    function updateFanCard($card_unit, unit_data) {
+        // 換気扇カード部分の更新
+        $status_bool = $card_unit.find(".status-bool")
+        $status_bool.removeClass('high-alert');
+        $status_bool.removeClass('low-alert');
+        if (unit_data["value"] === true) {
+            $status_bool.text("ON")
+            $status_bool.addClass('high-alert');
+        } else {
+            $status_bool.text("OFF")
+            $status_bool.addClass('low-alert');
+        }
+    }
+
+    function updateCO2Card($card_unit, unit_data) {
+        // Co2濃度計カード部分の更新
+        $co2_val = $card_unit.find(".co2-val")
+        $co2_val.text(unit_data["value"] + " ppm")
+
+        $co2_val.removeClass('high-alert');
+        if (unit_data["value"] >= 1000) {
+            $co2_val.addClass('high-alert');
+        }
+    }
+
+    function updateThermoHygroCard($card_unit, unit_data) {
+        // 温湿度計カード部分の更新
+        $card_unit.find(".get-temp").text(unit_data["temp"] + " ℃");
+        $card_unit.find(".get-humidity").text(unit_data["humidity"] + " %");
+    }
+
+    function fetchApiData() {
+        $.ajax({
+            url: '/get_dummy',
+            type: 'GET',
+            success: function(data) {
+                data.forEach(unit_data => {
+                    unit_id = unit_data["name"];
+                    $card_unit = $("#" + unit_id);
+                    
+
+                    if (unit_id.includes("air_conditioner")) {
+                        updateAirconCard($card_unit, unit_data);
+                    }
+
+                    if (unit_id.includes("fan")) {
+                        updateFanCard($card_unit, unit_data);
+                    }
+
+                    if (unit_id.includes("co2_monitor")) {
+                        updateCO2Card($card_unit, unit_data);
+                    }
+
+                    if (unit_id.includes("thermohygrometer")) {
+                        updateThermoHygroCard($card_unit, unit_data);
+                    }
+                });
+            },
+            error: function(error) {
+                console.log('API呼び出し中にエラーが発生しました:', error);
+            }
+        });
+    }
+
+    fetchApiData()
+    // 1000ミリ秒（1秒）ごとにfetchApiData関数を実行
+    setInterval(fetchApiData, 5000);
+});
