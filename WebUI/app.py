ce90e775f66deafd9472f42227be208709a5eb56
matsuda705 <koji.matsuda@jbs.com>
2024-07-11 01:44:26 +0000

    コメントアウト削除

--- WebUI/app.py
+++ WebUI/app.py
@@ -1,6 +1,6 @@
 import json
 import os
-# import requests
+import requests
 
 from flask import Flask, render_template, jsonify
 
@@ -15,13 +15,13 @@ def index():
     return render_template("index.html")
 
 
-# @app.route('/get_current_data')
-# def get_current_data():
-#     response = requests.get(
-#         "https://mk-flask-core-app.azurewebsites.net/get_current_data"
-#     )
+@app.route('/get_current_data')
+def get_current_data():
+    response = requests.get(
+        "https://mk-flask-core-app.azurewebsites.net/get_current_data"
+    )
 
-#     return jsonify(response.json())
+    return jsonify(response.json())
 
 
 @app.route('/get_dummy')
