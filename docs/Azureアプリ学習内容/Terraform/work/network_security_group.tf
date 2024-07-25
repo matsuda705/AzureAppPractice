70a92062560087f5aad09b8a20c4639febb227aa
matsuda705 <koji.matsuda@jbs.com>
2024-07-25 02:11:18 +0000

    nsgのファイル分け

--- docs/Azureアプリ学習内容/Terraform/work/network_security_group.tf
+++ docs/Azureアプリ学習内容/Terraform/work/network_security_group.tf
@@ -11,32 +11,12 @@ resource "azurerm_network_security_group" "web_nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_network_security_group" "gateway_nsg" {
-  name                = "mk-gateway_app-security-group-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_subnet_network_security_group_association" "funcNsgConnection" {
+#   subnet_id                 = azurerm_subnet.funcAppSubnet.id
+#   network_security_group_id = azurerm_network_security_group.func_nsg.id
+# }
 
-resource "azurerm_subnet_network_security_group_association" "funcNsgConnection" {
-  subnet_id                 = azurerm_subnet.funcAppSubnet.id
-  network_security_group_id = azurerm_network_security_group.func_nsg.id
-}
-
-resource "azurerm_subnet_network_security_group_association" "webNsgConnection" {
-  subnet_id                 = azurerm_subnet.webAppSubnet.id
-  network_security_group_id = azurerm_network_security_group.web_nsg.id
-}
-
-resource "azurerm_network_security_rule" "web_IP_nsr" {
-  name                       = "AllowJBSIP"
-  priority                   = 100
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "*"
-  source_port_range          = "*"
-  destination_port_range     = "*"
-  source_address_prefix      = "221.248.64.0/24"
-  destination_address_prefix = "*"
-  resource_group_name         = azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.web_nsg.name
-}
+# resource "azurerm_subnet_network_security_group_association" "webNsgConnection" {
+#   subnet_id                 = azurerm_subnet.webAppSubnet.id
+#   network_security_group_id = azurerm_network_security_group.web_nsg.id
+# }
--- docs/Azureアプリ学習内容/Terraform/work/virtual_network.tf
+++ docs/Azureアプリ学習内容/Terraform/work/virtual_network.tf
@@ -35,16 +35,3 @@ resource "azurerm_subnet" "funcAppSubnet" {
     }
   }
 }
-
-resource "azurerm_subnet" "gatewaySubnet" {
-  name                 = "gatewaySubnet-${random_integer.ri.result}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.3.0/24"]
-  delegation {
-    name = "Delegation"
-    service_delegation {
-      name = "Microsoft.Web/serverFarms"
-    }
-  }
-}
